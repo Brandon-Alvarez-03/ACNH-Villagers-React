700c4188c71e405ac7862486be9da6dabdb3c8a5
“Brandon <balvarez325@gmail.com>
2022-10-30 17:14:36 -0400

    set up structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 class="title">Animal Crossing New Horizon Villagers</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
